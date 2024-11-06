95bda059470574e4c772e9a7533b5175803b97b8
Youssef <yboudout84@gmail.com>
2024-11-06 15:22:36 +0100

    Swap these 2 arguments so they are in the correct order: expected value, actual value.

--- src/test/java/liste/ListeSimpleTest.java
+++ src/test/java/liste/ListeSimpleTest.java
@@ -51,7 +51,7 @@ class ListeSimpleTest {
         listeATester.ajout(2);
         listeATester.ajout(3);
         System.out.println(listeATester);
-        assertEquals(listeATester.toString(), "ListeSimple(Noeud(3), Noeud(2), Noeud(1))");
+        assertEquals("ListeSimple(Noeud(3), Noeud(2), Noeud(1))", listeATester.toString());
     }
 
     @Test
